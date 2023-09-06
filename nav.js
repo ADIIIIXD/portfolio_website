b4362d3fbbb2de647702be1bf8a1a0d5ddc1944d
aditya <addie4120@gmail.com>
2023-09-06 13:21:02 +0530

    hamburger edit

--- nav.js
+++ nav.js
@@ -1,8 +1,20 @@
+// JavaScript for the Hamburger Menu
 document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.getElementById("hamburger-menu");
-  const navList = document.getElementById("nav-list");
-
-  hamburger.addEventListener("click", function () {
-    navList.classList.toggle("active");
+    const hamburger = document.getElementById("hamburger-menu");
+    const navList = document.getElementById("nav-list");
+  
+    hamburger.addEventListener("click", function () {
+      navList.classList.toggle("active");
+    });
+  
+    // Add a click event listener to close the menu when clicking anywhere outside of it
+    document.addEventListener("click", function (event) {
+      if (
+        !navList.contains(event.target) &&
+        !hamburger.contains(event.target)
+      ) {
+        navList.classList.remove("active");
+      }
+    });
   });
-});
+  
